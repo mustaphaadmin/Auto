a41bf4fe0fdbcb18b4ff87b3ececc89798f24768
mustaphaadmin <50841167+mustaphaadmin@users.noreply.github.com>
2025-02-24 22:43:16 +0000

    Update rotlbc.py

--- ressources/kuw/rotlbc.py
+++ ressources/kuw/rotlbc.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_kwik_key_from_page(channel_id):
-    url = "https://rotana.net/en/channels"
+    url = "https://rotana.net/en/live"
     headers = {
         'Referer': 'https://rotana.net/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0',
@@ -54,20 +54,4 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 channel_id = sys.argv[1]
-media_url = channel_id.replace("-", "")
-kwik_key = get_kwik_key_from_page(channel_id)
-
-if kwik_key:
-    acl_token = get_channel_token(kwik_key, media_url)
-    if acl_token:
-        m3u8_link = construct_m3u8_link(media_url, acl_token)
-        with open(f"ressources/kuw/{media_url}.m3u8", "w") as file:
-            file.write("#EXTM3U\n")
-            file.write("#EXT-X-VERSION:3\n")
-            file.write(f"#EXT-X-STREAM-INF:BANDWIDTH=4265866,RESOLUTION=1920x1080,CODECS=\"avc1.640029,mp4a.40.2\",CLOSED-CAPTIONS=NONE\n{m3u8_link}\n")
-        print(response.text)
-        print(m3u8_link)
-    else:
-        print("Failed to retrieve ACL token")
-else:
-    print(f"Failed to retrieve kwik_key for channel {channel_id}")
+media_url = channel_id
